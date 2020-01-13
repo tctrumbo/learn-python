81a014d96d77ceb1ca0813a6e9af0d964bfc99c0
Travis Trumbo <tctrumbo@gmail.com>
2020-01-12 20:28:05 -0500

    refactor stock-market.py logic into function

--- stock-market.py
+++ stock-market.py
@@ -19,26 +19,33 @@
 # 18.88 19.03
 
 # tick = [19.35, 19.30, 18.88, 18.93, 18.95, 19.03, 19.00, 18.97, 18.97, 18.98]
-tick = [9.20, 8.03, 10.02, 8.08, 8.14, 8.10, 8.31, 8.28, 8.35, 8.34, 8.39, 8.45, 8.38, 8.38, 8.32, 8.36, 8.28, 8.28, 8.38, 8.48, 8.49, 8.54, 8.73, 8.72, 8.76, 8.74, 8.87, 8.82, 8.81, 8.82, 8.85, 8.85, 8.86, 8.63, 8.70, 8.68, 8.72, 8.77, 8.69, 8.65, 8.70, 8.98, 8.98, 8.87, 8.71, 9.17, 9.34, 9.28, 8.98, 9.02, 9.16, 9.15, 9.07, 9.14, 9.13, 9.10, 9.16, 9.06, 9.10, 9.15, 9.11, 8.72, 8.86, 8.83, 8.70, 8.69, 8.73, 8.73, 8.67, 8.70, 8.69, 8.81, 8.82, 8.83, 8.91, 8.80, 8.97, 8.86, 8.81, 8.87, 8.82, 8.78, 8.82, 8.77, 8.54, 8.32, 8.33, 8.32, 8.51, 8.53, 8.52, 8.41, 8.55, 8.31, 8.38, 8.34, 8.34, 8.19, 8.17, 8.16]
-length= len(tick)
+def determineMaxProfit( tick ):
+    length= len(tick)
 
-maxProfit=0.0
-for i in range(length):    
-        # print(tick[i])
-        try:                  
-            for x in range(i+1,length):                          
-                # print("nest" + str(x) +" "+ str(tick[x]))
-                profit = tick[x]-tick[i]
-                # print (tick[x],tick[i],tick[x]-tick[i])
-                if profit > maxProfit and x-i>1:
-                    maxProfit=profit
-                    minBuy=tick[i]
-                    maxSell=tick[x]
-                #print(str(tick[x])+ "-"+str(tick[i])+ "=" +str(profit))
+    maxProfit=0.0
+    for i in range(length):    
+            # print(tick[i])
+            try:                  
+                for x in range(i+1,length):                          
+                    # print("nest" + str(x) +" "+ str(tick[x]))
+                    profit = tick[x]-tick[i]
+                    # print (tick[x],tick[i],tick[x]-tick[i])
+                    if profit > maxProfit and x-i>1:
+                        maxProfit=profit
+                        minBuy=tick[i]
+                        maxSell=tick[x]
+                    #print(str(tick[x])+ "-"+str(tick[i])+ "=" +str(profit))
 
-        except IndexError:
-            pass
-        continue
-print("Min Buy: "+str(minBuy) + " Max Sell: "+str(maxSell) )
-print("Max Profit: "+str(maxProfit))
-    
+            except IndexError:
+                pass
+            continue
+    print("Min Buy: "+str(minBuy) + " Max Sell: "+str(maxSell) )
+    print("Max Profit: "+str(maxProfit))
+
+def main():
+   tick = [9.20, 8.03, 10.02, 8.08, 8.14, 8.10, 8.31, 8.28, 8.35, 8.34, 8.39, 8.45, 8.38, 8.38, 8.32, 8.36, 8.28, 8.28, 8.38, 8.48, 8.49, 8.54, 8.73, 8.72, 8.76, 8.74, 8.87, 8.82, 8.81, 8.82, 8.85, 8.85, 8.86, 8.63, 8.70, 8.68, 8.72, 8.77, 8.69, 8.65, 8.70, 8.98, 8.98, 8.87, 8.71, 9.17, 9.34, 9.28, 8.98, 9.02, 9.16, 9.15, 9.07, 9.14, 9.13, 9.10, 9.16, 9.06, 9.10, 9.15, 9.11, 8.72, 8.86, 8.83, 8.70, 8.69, 8.73, 8.73, 8.67, 8.70, 8.69, 8.81, 8.82, 8.83, 8.91, 8.80, 8.97, 8.86, 8.81, 8.87, 8.82, 8.78, 8.82, 8.77, 8.54, 8.32, 8.33, 8.32, 8.51, 8.53, 8.52, 8.41, 8.55, 8.31, 8.38, 8.34, 8.34, 8.19, 8.17, 8.16]
+   determineMaxProfit(tick) 
+
+
+if __name__ == '__main__':
+    main()
